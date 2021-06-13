3588cecea47676336bd2d8771286534b718ee8a0
gmprojects-ctrl <68788400+gmprojects-ctrl@users.noreply.github.com>
2021-06-13 18:57:54 +0100

    Added some stuff

--- C/MandelBrot/MandelBrot.c
+++ C/MandelBrot/MandelBrot.c
@@ -2,40 +2,40 @@
 #include<math.h>
 #include "Complex.h"
 
+
+
+
 int maxIter =80;
 
-
-int maxiteration(Complex z){
+int maxiteration(Complex* z,Complex* start,int p){
 	int Iter=0;
 	Complex result;
-	result.a=0;
-	result.b=0;
+	result.a=start->a;
+	result.b=start->b;
 	while(Iter < maxIter && cmodulus(&result) <= 2){
-		Complex z_2 = cmultiply(&result,&result);
-		result = cadd(&z_2,&z);
+		for(int i=1; i<p; i++){
+			result = cmultiply(&result,&result);
+		}
+		result = cadd(&result,z);
 		Iter +=1;
 	}
 	return Iter;
 }
-
-int main(){
-	double RSTART=-2;
-	double REND=1;
-	double ISTART=-1;
-	double IEND=1;
+int RenderImage(double RSTART,double REND, double ISTART, double IEND, int DIMX, int DIMY, int power, Complex start){
+	static int number =0;
 	double RSCALE= REND -RSTART;
 	double ISCALE= IEND -ISTART;
-	int DIMX = 1280;
-	int DIMY = 720;
-	FILE* image = fopen("image.ppm","wb");
-	FILE* diag = fopen("diag.txt","w");
+	char imagename[100];
+	sprintf(imagename,"image%d.ppm",number);
+	FILE* image = fopen(imagename,"wb");
+	FILE* diag = fopen("diag.txt","a");
 	fprintf(image,"P6\n%d %d\n255\n",DIMX,DIMY);
 	for(int j=0; j<DIMY; j++){
 		for(int i=0; i<DIMX;i++){
 			Complex current;
 			current.a = RSTART +   (double)i * RSCALE/DIMX ;
 			current.b = ISTART + (double)j  * ISCALE/DIMY;
-			double maxCurrent = (double)maxiteration(current);
+			double maxCurrent = (double) maxiteration(&current,&start,power);
 			char colour = round( (maxCurrent/maxIter)*255) ;
 			unsigned char pixel[]={colour,colour,colour};
 			fwrite(pixel,1,3,image);
@@ -46,6 +46,17 @@ int main(){
 	}
 	fclose(image);
 	fclose(diag);
+	number++;
+	return 0;
+}
+
+
+
+int main(){
+	Complex origin;
+	origin.a=0;
+	origin.b=0;
+	RenderImage(-2,2,-1,1,1280,720,2,origin);	
 	return 0;
 
 }

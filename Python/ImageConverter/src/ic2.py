c7c60757985d8cc4664d68df9012a9307e19fd89
gmprojects-ctrl <68788400+gmprojects-ctrl@users.noreply.github.com>
2021-06-22 18:02:10 +0100

    Another one

--- Python/ImageConverter/src/ic2.py
+++ Python/ImageConverter/src/ic2.py
@@ -5,24 +5,30 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 
+# Makes the fonts size proportional the the image
 def correctfontsize(imageheight):
     size = round((0.25 * imageheight / 8) * 6)
     return size
 
-
+# Converts Images to memes
 def ImageConverter(name, text, tolerance, colour_1, colour_2, text_colour):
-    with Image.open(name) as im:
+    with Image.open(name) as im:   # Opening the file as a Image Object
         pixelmap_original = im.load()
         image2 = Image.new(im.mode, im.size)
+        # Creating an empty ImageObject of the same height and length
         pixelmap_new = image2.load()
-        for i in range(im.size[0]):
+        for i in range(im.size[0]): 
+            # Goes through each pixel of the original image 
+            # and checks if the image fits a certain tolerance
             for j in range(im.size[1]):
                 pixel_value = round(sum(pixelmap_original[i, j]) / 3)
                 if pixel_value <= int(tolerance):
                     pixelmap_new[i, j] = colour_1
                 else:
                     pixelmap_new[i, j] = colour_2
-        font = ImageFont.truetype("impact.ttf",
+
+        # Writes text on the image
+        font = ImageFont.truetype("../../fonts/impact.ttf",
                                   correctfontsize(image2.size[1]))
         draw_obj = ImageDraw.Draw(image2)
         text_width = draw_obj.textlength(text, font)

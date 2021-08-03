d6ee4af43255a5c19a5355d29df6d52158e2dbac
gmprojects-ctrl <user@pc.localdomain>
2021-08-03 16:20:15 +0100

    Fixed Row Echelon

--- C/MathExtra/rowechelon.c
+++ C/MathExtra/rowechelon.c
@@ -1,78 +1,66 @@
 #include<stdio.h>
-int scmulr(double*start,int row,int column,int focus,double scalar)
+void scmulr(int column,int focus,double scalar,double arr[][column])
 {
-	if(focus>row-1 || focus < 0)
+	for(int j =0; j<column;j++)
 	{
-		return -1;
-	}else{
-		for(int j=0; j<column; j++){
-
-			*(start+row*focus+j)*=scalar;
-		}
-		return 0;
+		arr[focus][j]*=scalar;
 	}
+	
 
 }
-void swaprow(double* start, int row, int column, int row_1, int row_2)
+void swaprow(int column,int row_1, int row_2, double arr[][column])
 {
 	double temp[column];
 	for(int i=0; i<column;i++)
 	{
-		temp[i]=*(start+row_2*row+i);
+		temp[i]=arr[row_2][i];
 	}
 	for(int i=0; i<column;i++)
 	{
-		*(start+row_2*row+i)=*(start+row_1*row+i);
+		arr[row_2][i]=arr[row_1][i];
 	}
 	for(int i=0; i<column;i++){
-		*(start+row_1*row+i)=temp[i];
+		arr[row_1][i]=temp[i];
 	}
 
 }
-void addrow(double* start, int row, int column, int row_1, int row_2)
+void addrow(int column, int row_1, int row_2,double scalar,double arr[][column])
 {
 	for(int i=0; i<column;i++)
 	{
-		*(start+row_2*row+i)+=*(start+row_1*row+i);
+		arr[row_2][i]+=arr[row_1][i]*scalar;
 	}
 
 }
-void printmatrix(double* start, int row, int column)
+void printmatrix( int row, int column, double arr[][column])
 {
 	for(int i=0; i < row; i++)
 	{
 		for(int j =0; j<column;j++)
 		{
 
-			printf("%f ",*(start+i*row+j));
+			printf("%f ",arr[i][j]);
 		}
 		printf("\n");
 	}
 }
-void gaussianelim(double* start, int row, int column){
+void gaussianelim( int row, int column, double arr[][column] ){
 	int i =0;
 	int j=0;
 	while(i<row && j<column ){
-		double focus_value=*(start+i*row+j);
+		double focus_value=arr[i][j];
 		if(focus_value == 0)
 		{
 			j+=1;
 		}else{
-			printf("%f\n",focus_value);
-			scmulr(start,row,column,i,1/focus_value);
-			printmatrix(start,row,column);
-			printf("\n");
+			scmulr(column,i,1/focus_value,arr);
 			for(int z=0; z< row;z++)
 			{
 				if(z==i){}
 				else{
-					for(int y=0; y<column;y++)
-					{
-						*(start+z*row+y)-=*(start+i*row+y)*(*(start+z*row+j));
-					}
+					addrow(column,i,z,-arr[z][j],arr);
+
 				}
-					printmatrix(start,row,column);
-					printf("\n");
 			}
 			i+=1;
 			j+=1;
@@ -85,8 +73,12 @@ int main()
 		{1,2,1},
 		{3,4,12},
 	};
-	gaussianelim(&a[0][0],2,3);
-printmatrix(&a[0][0],2,3);
+	gaussianelim(2,3,a);
+	printmatrix(2,3,a);
+	double b[][4]={{2,-3,0,-6},{1,-1,1,1},{0,-3,-2,-5}};
+	printf("\n");
+	gaussianelim(3,4,b);
+	printmatrix(3,4,b);
 	return 0;
 
 }
